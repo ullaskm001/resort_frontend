d8b2106314bd524d2a6b4fae9de76354dd821b68
ullas <ullaskaruvatumana@gmail.com>
2025-11-15 23:59:04 +0530

    some more changes

--- src/components/AdminBookings.jsx
+++ src/components/AdminBookings.jsx
@@ -5,7 +5,7 @@ const AdminBookings = () => {
   const [bookings, setBookings] = useState([]);
 
   useEffect(() => {
-    axios.get("${import.meta.env.VITE_API_URL}/api/bookings")
+    axios.get(`${import.meta.env.VITE_API_URL}/api/bookings`)
       .then(res => setBookings(res.data))
       .catch(err => console.log(err));
   }, []);
